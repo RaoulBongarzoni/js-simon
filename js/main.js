adf56dfb795eedd1892f1fee5727f721e6ac43c1
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-11-10 10:20:15 +0100

    REFACT: code polisher 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-//genero 5 numeri casuali
-    //funzione math random
-    //mostro a schermo i 5 numeri
 
 
 const random=random5();
 var my_array=[];
 const name_helper=["primo", "secondo", "terzo", "quarto", "quinto"]
 
-
-
+//genero 5 numeri casuali
+    //funzione math random
+    //mostro a schermo i 5 numeri
 for (let i = 0; i<5 ; i++){
 
     document.getElementById("numeri").innerHTML += ` <span class="text-white"> ${random[i]} </span>`;
@@ -20,8 +18,6 @@ setTimeout(cleanScreen, 10000);  //30 secondi era troppo tempo :)
 
 //chiedo uno alla volta i numeri visualizzati
 setTimeout(number_in, 10100);
-
-
 
 //mostro a schermo quanti numeri erano corretti
     //interazione html
